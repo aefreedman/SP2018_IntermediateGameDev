e9b0bd6f8380a606a704fc11eb4ec71255634e97
Aaron Freedman <aaron@secretcrush.net>
2018-01-29 12:23:11 -0500

    Week 1 project

---
+++ Week01/Assets/Scripts/MoveBall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    private Vector3 startDirection;
+
+    // Use this for initialization
+    void Start()
+    {
+        startDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(startDirection);
+    }
+}
---
+++ Week01/Assets/Scripts/MovePaddle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// every frame change the transform position according to input
+		
+		// get input
+//		var up = Input.GetKeyDown(KeyCode.W);
+//		var down = Input.GetKeyDown(KeyCode.S);
+		
+		var up = Input.GetKey(KeyCode.W);
+		var down = Input.GetKey(KeyCode.S);
+		
+		var speed = 0.1f;
+
+		// change transform -- these are all equivalent
+//		var t = GetComponent<Transform>();
+//		var t2 = transform;
+//		var t3 = gameObject.transform;
+		
+//		transform.position = new Vector3(0, 0, 0);
+
+		if (up)
+			transform.Translate(Vector3.up * speed);
+		if (down)
+			transform.Translate(Vector3.down * speed);
+
+
+	}
+}
