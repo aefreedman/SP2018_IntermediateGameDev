237787e3ba8ba32d01f9f8581f0519931476ae2c
Aaron Freedman <aaron@secretcrush.net>
2018-02-26 12:00:01 -0500

    Added debug helper delegate

---
+++ Week01/Assets/Scripts/DebugHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public static class DebugHelper
+{
+    public static Action DebugMessage(string message, string color)
+    {
+        return delegate { Debug.LogFormat("<color={1}>{0}</color>", message, color); };
+    }
+}
