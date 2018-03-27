5d9b6e6d76d676e56fcf8528b5d42cf041be3fb5
Aaron Freedman <aaron@secretcrush.net>
2018-03-27 11:18:39 -0400

    Scriptable object example

---
+++ Week01/Assets/Scripts/ScriptableObject/ExampleScriptableObjectUser.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace ScriptableObject
+{
+    public class ExampleScriptableObjectUser : MonoBehaviour
+    {
+        public GameSettings GameSettings;
+
+        public void Start()
+        {
+            Debug.Log(GameSettings.PlayerSettings.SomeFloat.ToString("F1"));
+        }
+    }
+}
---
+++ Week01/Assets/Scripts/ScriptableObject/GameSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace ScriptableObject
+{
+    [CreateAssetMenu(fileName = "GameSettings", menuName = "Settings/GameSettings")]
+    public class GameSettings : UnityEngine.ScriptableObject
+    {
+        public Settings.Player PlayerSettings;
+        public Settings.Player PlayerSettings2;
+        public Settings.Enemies EnemySettings;
+    }
+
+    public class Settings
+    {
+        [Serializable]
+        public class Player
+        {
+            public int SomeNumber;
+            public float SomeFloat;
+            
+        }
+        
+        [Serializable]
+        public class Enemies
+        {
+            public int Health;
+        }
+    }
+}
