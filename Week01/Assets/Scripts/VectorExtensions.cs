93a3363d468cd7175befca7814ba99789619bfb4
Aaron Freedman <aaron@secretcrush.net>
2018-04-02 19:06:35 -0400

    added vector extensions

---
+++ Week01/Assets/Scripts/VectorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public static class VectorExtensions
+{
+    public static Vector2 RandomVector2(Vector2 minVector, Vector2 maxVector)
+    {
+        return new Vector2(Random.Range(minVector.x, maxVector.x), Random.Range(minVector.y, maxVector.y));
+    }
+
+    public static Vector3 RandomVector3(float minVector, float maxVector)
+    {
+        return new Vector3(Random.Range(minVector, maxVector), Random.Range(minVector, maxVector), Random.Range(minVector, maxVector));
+    }
+}
